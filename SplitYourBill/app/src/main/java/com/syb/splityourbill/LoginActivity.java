953669ca464ce58a91bda8bd123e7751bbe96176
asayushg <asayushg@gmail.com>
2018-12-12 23:11:38 +0530

    login integrated with firebase

--- SplitYourBill/app/src/main/java/com/syb/splityourbill/LoginActivity.java
+++ SplitYourBill/app/src/main/java/com/syb/splityourbill/LoginActivity.java
@@ -1,16 +1,23 @@
 package com.syb.splityourbill;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
 
     private String email,pass;
     private EditText emailField_login,password;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +25,7 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         emailField_login =(EditText) findViewById(R.id.emailField_login);
         password = (EditText) findViewById(R.id.password);
+        auth = FirebaseAuth.getInstance();
 
     }
 
@@ -30,7 +38,16 @@ public class LoginActivity extends AppCompatActivity {
         email = emailField_login.getText().toString();
         pass = password.getText().toString();
         if( !email.equals("") && !pass.equals("")){          // firebase code
-
+                auth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this,"Logged In Successfully.",Toast.LENGTH_SHORT).show();
+                            //here start profile activity
+                        }
+                        else Toast.makeText(LoginActivity.this,task.getException().getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                });
         }
         else{                                  // phone or pass is not null CHECK
             Toast.makeText(this,"Please Enter valid phone or password",Toast.LENGTH_SHORT).show();
